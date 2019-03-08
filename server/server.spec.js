3cce9e65c2a403bc52e32562da05e0b49c90a494
Trai Lynne <trailynne@gmail.com>
2019-03-08 12:39:55 -0500

    Defined Tests for server.js Checks repsonse status, content type, and content

--- server/server.spec.js
+++ server/server.spec.js
@@ -1,1 +1,25 @@
+const request = require('supertest');
 
+const server = require('./server');
+
+describe('server.js', () => {
+    describe('GET /', () => {
+        it('should return 200 ok', async () => {
+            const resp = await request(server).get('/');
+
+            expect(resp.status).toBe(200);
+        });
+
+        it('should return Text', () => {
+            const resp = await request(server).get('/');
+
+            expect(resp.type).toBe('text/html')
+        });
+
+        it('should return `Welcome to the Main API`', async () => {
+            const resp = await request(server).get('/');
+
+            expect(resp.body).toEqual('Welcome to the Main API');
+        });
+    });
+});
